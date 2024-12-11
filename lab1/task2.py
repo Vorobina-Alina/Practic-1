f0433fb4b3df5800e8c61ec4246d82172f83e403
Vorobina-Alina <151908614+Vorobina-Alina@users.noreply.github.com>
2024-12-11 15:53:47 +0000

    Lab 1 correct

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 import random
+import sys
 
-a = int(input())
-print(f"A = {a}")
-b = random.randint(-10, 10)
-print(f"B = {b}")
-if b == 0: print("Error")
-else:         
-   result = a/b
-   print(f"A/B = {result}")
+def main():
+   try:
+      data = input()
+      A = int(data)
+      B = random.randint(-10, 10)
+      
+      res = A / B
+   except ValueError:
+      print('Wrong value', file=sys.stderr)
+   except ZeroDivisionError:
+      print('Error! B=0', file=sys.stderr)
+   else:
+      print(res)
+
+if __name__ == "__main__":
+   main()
--- lab1/task3.py
+++ lab1/task3.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 
+import sys
 import math
 
-num = float(input())
-sqrt_num = math.sqrt(num)
-with open("output.txt", "w") as f:
-   f.write(str(sqrt_num) + "\n")
+def main():
+    try:
+        data = input()
+        num = float(data)
+        
+        if num < 0:
+            raise Exception('Error! Value <0')
+
+    except EOFError:
+        print('Error!', file=sys.stderr)
+    except ValueError:
+        print('Error! Value is wrong!', file=sys.stderr)
+    except Exception as e:
+        print(e, file=sys.stderr)
+    else:
+        sqrt_num = math.sqrt(num)
+        print(f"Квадратный корень: {sqrt_num}")
+
+if __name__ == "__main__":
+    main()
